2003e0254ccc8fd2f1c1d1d26baecd7453b4dc91
benlefr <37343685+benlefr@users.noreply.github.com>
2020-12-20 17:33:03 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    
+    console.log("client is ready");
+})
+client.on('message', message =>{
+    if(message.author.bot) return;
+    if(message.author.id === "369907060038565899")
+    {
+        message.reply("TG")
+    }
+})
+client.login(process.env.TOKEN)
