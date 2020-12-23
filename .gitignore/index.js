b653e473298ec04fa379b75c11c341c85638563a
benlefr <37343685+benlefr@users.noreply.github.com>
2020-12-24 00:02:20 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,6 +9,7 @@ client.on('message', message =>{
     if(message.author.bot) return;
     if(message.author.id === "369907060038565899" || message.author.tag === "Clara#6724")
     {
+        message.channel.send("TG Clara ")
         message.reply("TG")
     }
 })
